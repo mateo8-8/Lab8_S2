5e02baff051b99f94dfb825c27467f6eacbc0d2b
mateo8-8 <68440461+mateo8-8@users.noreply.github.com>
2021-11-06 16:19:01 -0400

    Add files via upload

---
+++ hero.js
@@ -0,0 +1,78 @@
+class SuperHuman {
+	constructor(name, powerLevel) {
+		this.name = name;
+		this.powerLevel = powerLevel;
+	}
+}
+
+// Define SuperHero and SuperVillain classes here
+class SuperVillain extends SuperHuman {
+	constructor(name, alias, powerLevel){
+		super(name, powerLevel);
+		this.alias = alias;
+	}
+	getEvilChuckle(){
+		return "Ha ha ha!";
+	}
+}
+
+class SuperHero extends SuperHuman {
+	constructor(name, alias, powerLevel){
+		super(name, powerLevel);
+		this.alias = alias;
+	}
+	battle(SV){
+		if (this.powerLevel >= SV.powerLevel){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+
+const heroes = {
+	"Super Bacon": new SuperHero("Jack Oinker", "Super Bacon", 2),
+	"Flat-Man": new SuperHero("Peter Pranker", "Flat-Man", 5),
+	"Mighty Woman": new SuperHero("Diana Dense", "Mighty Woman", 8),
+	"Captain Marvelous": new SuperHero("Carol Hangers", "Captain Marvelous", 9)
+};
+
+const villains = {
+	"The Jokester": new SuperVillain("Jack Nastier", "The Jokester", 3),
+	"Magnet Man": new SuperVillain("Max Eisenhardt", "Magnet Man", 6),
+	"Lex Loner": new SuperVillain("Lex Loner", "Lex Loner", 2),
+	"Thankos": new SuperVillain("Thankos", "Thankos", 9)
+};
+
+registerHandlers();
+
+function registerHandlers() {
+	// Detect selection of hero and villain
+	document.querySelector("#heroSelect").addEventListener("change", selectionChanged);
+	document.querySelector("#villainSelect").addEventListener("change", selectionChanged);
+
+	selectionChanged();
+}
+
+function selectionChanged() {
+	const selectedHeroValue = document.querySelector("#heroSelect").value;
+	const selectedVillainValue = document.querySelector("#villainSelect").value;
+
+	// Get hero and villain selected
+	const selectedHero = heroes[selectedHeroValue];
+	const selectedVillain = villains[selectedVillainValue];
+
+	// Your code goes here
+	let re = selectedHero.battle(selectedVillain);
+	let sl = document.getElementById("winner");
+	
+	if (re === false){
+		sl.innerHTML = '<p>' +  'Winner: ' + selectedVillain.alias + '</p>';
+	} else {
+		sl.innerHTML = '<p>' + 'Winner: ' +  selectedHero.alias + '</p>';
+	}
+
+	
+	
+}
